89b41f9ffee5c3a7195328268eed7d7f7997e6b4
jcarrharris <joncarrharris@gmail.com>
2013-10-07 15:16:37 -0400

    getting ready for migration

--- db/migrate/20131007190841_create_businesses.rb
+++ db/migrate/20131007190841_create_businesses.rb
@@ -1,6 +1,12 @@
 class CreateBusinesses < ActiveRecord::Migration
   def change
     create_table :businesses do |t|
+    	t.string "name"
+    	t.string "address"
+    	t.integer "phone_number"
+    	t.string "website_url"
+    	t.integer "hours_of_operation"
+    	t.string "payment_methods"
 
       t.timestamps
     end
