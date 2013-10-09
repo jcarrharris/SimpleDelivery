0e7bb305345ce3cdb9e210a013cea6d058aa7355
jcarrharris <joncarrharris@gmail.com>
2013-10-09 01:14:42 -0400

    added profile picture for user

---
+++ db/migrate/20131009051133_add_user_image_to_user.rb
@@ -0,0 +1,5 @@
+class AddUserImageToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131009041126) do
+ActiveRecord::Schema.define(version: 20131009051133) do
 
   create_table "businesses", force: true do |t|
     t.string   "business_name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20131009041126) do
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
