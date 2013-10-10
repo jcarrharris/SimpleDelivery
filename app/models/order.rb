995c533d52f4afb4eca624adc57002103781512c
cshah3 <shah.control@gmail.com>
2013-10-09 20:07:46 -0400

    fixed order geocoder

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,7 @@ class Order < ActiveRecord::Base
 
   validates_presence_of :tracking_number, :delivery_address, :phone_number, :length, :width, :height, :weight, :quantity, :declared_value, :packaging, :pickup_time, :delivery_time
   validates_uniqueness_of :tracking_number
+
+  geocoded_by :delivery_address              # can also be an IP address
+  after_validation :geocode, :if => :delivery_address_changed?  # auto-fetch coordinates
 end
