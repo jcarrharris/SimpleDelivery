a7d77ed09f95bf1ddb7337995a38f60ed31e8ebc
jcarrharris <joncarrharris@gmail.com>
2013-10-10 18:34:07 -0400

    fixed user table

--- db/migrate/20131010222538_devise_create_user_redux.rb
+++ db/migrate/20131010222538_devise_create_user_redux.rb
@@ -1,5 +1,6 @@
-class DeviseCreateUsers < ActiveRecord::Migration
+class DeviseCreateUserRedux < ActiveRecord::Migration
   def change
+    drop_table :users
     create_table(:users) do |t|
       t.string :first_name
       t.string :last_name
