2100778908cb02c2902ac7209bef2c058d781efd
cshah3 <shah.control@gmail.com>
2013-10-10 14:36:49 -0400

    rerouting single business owners

--- app/models/business.rb
+++ app/models/business.rb
@@ -4,6 +4,7 @@ class Business < ActiveRecord::Base
 	belongs_to :user
 
 	validates_presence_of :business_name, :website_url, :phone_number
+  validates :phone_number, length: { is: 10 }
   
   mount_uploader :image, ImageUploader
 end
