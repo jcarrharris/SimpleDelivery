82548b6b2b5da17bbafa2fdb38a33470243f2ea1
cshah3 <shah.control@gmail.com>
2013-10-14 15:41:13 -0400

    fixed welcome error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
 	
 	def index
-    redirect_to businesses_path if current_user.role == "Merchant"
-    redirect_to orders_path if current_user.role == "Courier"
+    if user_signed_in?
+      redirect_to businesses_path if current_user.role == "Merchant"
+      redirect_to orders_path if current_user.role == "Courier"
+    else
+    end
 	end
 end
