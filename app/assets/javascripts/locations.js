d882080e853c7e949bc29d63cde64a273a7c4a86
cshah3 <shah.control@gmail.com>
2013-10-18 17:07:29 -0400

    fixed js files

--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,11 +1,13 @@
-var myOptions = {
-  zoom: 16,
-  center: new google.maps.LatLng($('#locationMap').data('latitude'),$('#locationMap').data('longitude')),
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map(document.getElementById("locationMap"), myOptions);
-var marker = new google.maps.Marker({
-  map: map,
-  position: new google.maps.LatLng($('#locationMap').data('latitude'),$('#locationMap').data('longitude')),
-  title: "Location Address"
+$(document).ready( function() {
+  var myOptions = {
+    zoom: 16,
+    center: new google.maps.LatLng($('#locationMap').data('latitude'),$('#locationMap').data('longitude')),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("locationMap"), myOptions);
+  var marker = new google.maps.Marker({
+    map: map,
+    position: new google.maps.LatLng($('#locationMap').data('latitude'),$('#locationMap').data('longitude')),
+    title: "Location Address"
+  });
 });
