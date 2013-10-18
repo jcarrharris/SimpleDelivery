dffa3c6aec29a6f865ad43089ac586dbf641f8ba
jcarrharris <joncarrharris@gmail.com>
2013-10-17 23:00:44 -0400

    adding jquery to form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -38,6 +38,43 @@ $(function() {
   $( "#accordion" ).accordion({ collapsible: true });
   $(this).addClass('newClass');
 
-  $('.phone_us').mask('(000) 000-0000');
-
+  $('#phone-number')
+    .keydown(function (e) {
+      var key = e.charCode || e.keyCode || 0;
+      $phone = $(this);
+      // Auto-format- do not expose the mask as the user begins to type
+      if (key !== 8 && key !== 9) {
+        if ($phone.val().length === 4) {
+          $phone.val($phone.val() + ')');
+        }
+        if ($phone.val().length === 5) {
+          $phone.val($phone.val() + ' ');
+        }
+        if ($phone.val().length === 9) {
+          $phone.val($phone.val() + '-');
+        }
+      }
+      // Allow numeric (and tab, backspace, delete) keys only
+      return (key == 8 || 
+          key == 9 ||
+          key == 46 ||
+          (key >= 48 && key <= 57) ||
+          (key >= 96 && key <= 105)); 
+    })
+    .bind('focus click', function () {
+      $phone = $(this);
+      if ($phone.val().length === 0) {
+        $phone.val('(');
+      }
+      else {
+        var val = $phone.val();
+        $phone.val('').val(val); // Ensure cursor remains at the end
+      }
+    })
+    .blur(function () {
+      $phone = $(this);
+      if ($phone.val() === '(') {
+        $phone.val('');
+      }
+    });
 });
